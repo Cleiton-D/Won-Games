777b5a9a3298fd44340336ac02984f110fbe08e1
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-08-30 18:46:34 -0400

    Configure styled-components Theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,13 @@
-import GlobalStyles from '../src/styles/global'
+import { ThemeProvider } from 'styled-components';
+
+import theme from '../src/styles/theme';
+import GlobalStyles from '../src/styles/global';
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
-]
+];
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,25 @@
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 
 import GlobalStyles from 'styles/global';
+import theme from 'styles/theme';
 
-function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  );
-}
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <ThemeProvider theme={theme}>
+    <Head>
+      <title>React Avançado - Boilerplate</title>
+      <link rel="shortcut icon" href="/img/icon-512.png" />
+      <link rel="apple-touch-icon" href="/img/icon-512.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta
+        name="description"
+        content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+      />
+    </Head>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
 export default App;
