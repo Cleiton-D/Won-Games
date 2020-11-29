6296cd8cc64a0f64f23a1856a31e3b421226cd17
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-11-29 16:13:59 -0400

    Create Home Template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,14 @@ import Head from 'next/head';
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
     <Head>
-      <title>React Avançado - Boilerplate</title>
+      <title>Won Games</title>
       <link rel="shortcut icon" href="/img/icon-512.png" />
       <link rel="apple-touch-icon" href="/img/icon-512.png" />
       <link rel="manifest" href="/manifest.json" />
-      <meta
-        name="description"
-        content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-      />
+      <meta name="description" content="The best Game Store in the world!" />
     </Head>
     <GlobalStyles />
     <Component {...pageProps} />
