d7f26b9a7707499bbc911b06242e301052ce0cd2
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-12-06 18:10:24 -0400

    Create Auth page template

---
+++ src/templates/Auth/Auth.spec.tsx
@@ -0,0 +1,40 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import matchMedia from '../../../.jest/match-media-mock';
+
+import Auth from '.';
+
+describe('<Auth />', () => {
+  beforeAll(() => {
+    matchMedia.useMediaQuery('(min-width: 768px)');
+  });
+
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    );
+
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2);
+
+    expect(
+      screen.getByRole('heading', {
+        name: /all your favorite games in one place/i
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform/i
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /auth title/i })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+});
---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,45 @@
+import Heading from 'components/Heading';
+import Logo from 'components/Logo';
+import MediaMatch from 'components/MediaMatch';
+
+import * as S from './styles';
+
+type AuthProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const Auth = ({ title, children }: AuthProps) => (
+  <S.Wrapper>
+    <MediaMatch greatherThan="medium">
+      <S.BannerBlock>
+        <S.BannerContent>
+          <Logo />
+
+          <div>
+            <Heading>All your favorite games in one place</Heading>
+            <S.Subtitle>
+              <strong>WON</strong> is the best and most complete gaming platform
+            </S.Subtitle>
+          </div>
+
+          <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+        </S.BannerContent>
+      </S.BannerBlock>
+    </MediaMatch>
+
+    <S.Content>
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
+
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+
+        {children}
+      </S.ContentWrapper>
+    </S.Content>
+  </S.Wrapper>
+);
+
+export default Auth;
