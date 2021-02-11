b4b051762ef9613c49962085fa4bafd2e4c400bb
Cleiton Dione Ahnerth Kiper <cleiton.kiper@trademaster.com.br>
2021-02-10 21:41:03 -0400

    create GameInfo component

---
+++ src/components/GameInfo/mock.ts
@@ -0,0 +1,6 @@
+export default {
+  title: 'Borderlands 3',
+  price: '215,00',
+  description:
+    'Experience the epic space strategy games that redefined the RTS genre. Control yout fleet and build as armada across more athan 30 single-player missions.'
+};
