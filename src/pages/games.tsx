a9b550ab76e5b48f11e3784d4540f12df2686b1a
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-02-21 14:56:41 -0400

    get games from api

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,6 +1,8 @@
-import Games, { GamesProps } from 'templates/Games';
+import { initializeApollo } from 'utils/apollo';
+import { QUERY_GAMES } from 'graphql/queries/games';
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames';
 
-import gamesMock from 'components/GameCardSlider/mock';
+import Games, { GamesProps } from 'templates/Games';
 
 import filterItemsMock from 'components/ExploreSidebar/mock';
 
@@ -8,11 +10,29 @@ export default function GamesPage(props: GamesProps) {
   return <Games {...props} />;
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+    query: QUERY_GAMES,
+    variables: {
+      limit: 9
+    }
+  });
+
   return {
     props: {
+      revalidate: 60,
       filterItems: filterItemsMock,
-      games: gamesMock
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        image: `http://127.0.0.1:1337${game.cover!.url}`,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      }))
     }
   };
 }
