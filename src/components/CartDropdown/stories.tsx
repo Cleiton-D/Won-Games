efa8e6d0efee35b43f239247f89e0a27a3fdcbf6
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-02-20 21:37:17 -0400

    create CartDropdown component

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,35 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import CartDropdown, { CartDropdownProps } from '.';
+
+import mockItems from 'components/CartList/mock';
+
+export default {
+  title: 'CartDropdown',
+  component: CartDropdown,
+  args: {
+    items: mockItems,
+    total: 'R$ 300,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta;
+
+export const Default: Story<CartDropdownProps> = (args) => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown {...args} />
+  </div>
+);
+
+export const Empty: Story<CartDropdownProps> = (args) => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown {...args} />
+  </div>
+);
+
+Empty.args = {
+  items: undefined,
+  total: undefined
+};
