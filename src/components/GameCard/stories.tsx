6e28f333ad4ee61b472350bc34ff9d1d9d68b720
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-02-21 15:09:33 -0400

    add game link to GameCard

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -6,6 +6,7 @@ export default {
   component: GameCard,
   args: {
     title: 'Population Zero',
+    slug: 'population-zero',
     developer: 'Rockstar Games',
     image: 'https://source.unsplash.com/user/willianjusten/300x140',
     price: 'R$ 235,00',
